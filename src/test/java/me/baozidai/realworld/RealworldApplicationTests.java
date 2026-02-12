739be32c81125a3f4ff92273038352d0a2f03864
baozidai <baozidai@outlook.com>
2026-02-12 15:12:58 +0800

    init commit

---
+++ src/test/java/me/baozidai/realworld/RealworldApplicationTests.java
@@ -0,0 +1,13 @@
+package me.baozidai.realworld;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RealworldApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
